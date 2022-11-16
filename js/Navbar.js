288ec1bf711e8ead52a0270097ab838d68253893
Levi <levicsnyder@gmail.com>
2022-11-15 20:10:23 -0500

    Starting site

---
+++ js/Navbar.js
@@ -0,0 +1,28 @@
+var params = document.body.getElementsByTagName('script');
+var query = params[0].classList;
+var parentFolder = query[0];
+
+var imageLocation;
+var jsaLocation;
+var mainLocation;
+
+if(parentFolder == "noParent")
+{
+    mainLocation = "";
+    imageLocation = "images/"
+}
+
+document.write(
+'<div class="container-fluid">' +
+    '<a class="navbar-brand" href="' + mainLocation + 'index.html"><img src = "'+ imageLocation +'UP.png" title = "Aqua Hydrocity" alt = "Aqua Hydrocity" width = "70" height = "70"/></a>'+
+    '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+        '<span class="navbar-toggler-icon"></span>' +
+    '</button>' +
+    '<div class="collapse navbar-collapse" id="navbarSupportedContent"> ' +
+        '<ul class="navbar-nav me-auto my-2 my-lg-0 " style="--bs-scroll-height: 100px;"> ' +
+            '<li class="nav-item"><a class="nav-link active" aria-current="page" href="index.html">Unbalanced Powers</a></li>' +
+            '<li class="nav-item"><a class="nav-link active" aria-current="page" href="creationStory.html">Birth Of The Universe</a></li>' +
+            '<li class="nav-item"><a class="nav-link active" aria-current="page" href="deities.html">Deities</a></li>' +
+        '</ul>' +
+    '</div>' +
+'</div>');
