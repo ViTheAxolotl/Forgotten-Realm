20d421a305443ce46341a75fab3eb55ebcb96c75
Levi <levicsnyder@gmail.com>
2023-01-16 16:45:50 -0500

    Test whatever

--- src/fire.js
+++ src/fire.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "../firebase/app";
-import { getFirestore, collection, addDoc } from '../firebase/firestore';
+import { initializeApp } from "../node_modules/firebase/app";
+import { getFirestore, collection, addDoc } from '../node_modules/firebase/firestore';
 
 
 const firebase = require("firebase");
