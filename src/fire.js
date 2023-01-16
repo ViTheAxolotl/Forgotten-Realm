1387b1f459bb4e03f945641158c698f17225b75d
Levi <levicsnyder@gmail.com>
2023-01-16 17:00:10 -0500

    shut up

--- src/fire.js
+++ src/fire.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
-import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js';
+import { getFirestore, collection, add } from 'https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js';
 
 
 const firebase = require("firebase");
