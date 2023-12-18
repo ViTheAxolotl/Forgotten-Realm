6f4382cf5dcefb8f2e05243e53f1d97dcff72d23
Vi Snyder <ViTheAxolotl@gmail.com>
2023-12-18 13:02:13 -0500

    maybe?

---
+++ js/specificMap.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function init()
+{
+    let titles = document.getElementsByClassName("mapTitle");
+    let img = document.getElementById("mapSource");
+    let htmlInfo = window.location.href;
+    let mapName = "";
+
+    htmlInfo = htmlInfo.split("?");
+    mapName = htmlInfo[1];
+    
+    titles.forEach(title =>
+    {
+        title.innerHTML = mapName;
+    });
+
+    img.src = mapImg[mapName];
+}
+
+let mapImg = 
+{
+    "Havenport" : "../images/maps/mapOfHavenport.png",
+    "Salatude" : "../images/maps/mapOfSalatude.png"
+};
