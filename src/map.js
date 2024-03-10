559bf8185387d15a10359e1a02e97cb9f666776e
Vi Snyder <ViTheAxolotl@gmail.com>
2024-03-10 19:36:09 -0400

    ???

--- src/map.js
+++ src/map.js
@@ -119,7 +119,7 @@ function addCharacter(character, update)
         char[0].src = `images/map/tokens/${character["name"]}.png`;
         char[0].id = character["name"];
         char[0].title = `${character["name"]}:`;
-        char[0].classList = `tokens ${character["name"]} ${character["name"]}`;
+        char[0].classList = `tokens ${character["name"]} char`;
         char[1].src = `images/map/tokens/${character["border"]}Border.png`;
         char[1].id = character["border"];
         char[1].classList = `tokens ${character["name"]} border}`;
