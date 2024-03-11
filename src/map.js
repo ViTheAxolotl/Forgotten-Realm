b18afd1a8acdd14ab9f4a4f6d2a243cb70c262c3
Vi Snyder <ViTheAxolotl@gmail.com>
2024-03-11 17:34:04 -0400

    adding

--- src/map.js
+++ src/map.js
@@ -90,7 +90,7 @@ function addTokens()
     let currentTokens = document.getElementsByClassName("tokens");
     if(currentTokens.length != 0)
     {
-        temp.innerHTML = 0;
+        temp.innerHTML = "0";
         for(let token of currentTokens)
         {
             if(!token.classList.contains("update"))
