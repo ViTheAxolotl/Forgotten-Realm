7eb121709aafa55bc7b532c73f4df6c2703d97dc
Vi Snyder <ViTheAxolotl@gmail.com>
2024-03-11 20:21:22 -0400

    New selection

--- js/selection.js
+++ js/selection.js
@@ -1,43 +1,116 @@
 "use strict";
-
+let enter = document.getElementById("enter");
+let charName = document.getElementById("name");
+let currentName;
 let div = document.getElementById("story");
 let characters = ["nook", "nibbly", "leonier", "razor"];
 let borders = ["blue", "golden", "green", "grey", "orange", "pink", "purple", "red"];
 let char = document.createElement("h3");
 let bord = document.createElement("h3");
 let go = document.createElement("button");
-char.innerHTML = "Select Character";
-char.classList = "blo";
-bord.innerHTML = "Select Boarder";
-bord.classList = "blo";
-go.innerHTML = "Go!";
-go.classList = "blo";
-go.onclick = handleGoButton;
-
-for(let i = 0; i < 4; i++)
-{
-    let name = characters[i];
-    characters[i] = document.createElement("img");
-    characters[i].src = `images/map/tokens/${name}.png`;
-    characters[i].id = name;
-    characters[i].onclick = select;
-}
-
-for(let i = 0; i < 8; i++)
-{
-    let color = borders[i];
-    borders[i] = document.createElement("img");
-    borders[i].src = `images/map/tokens/${color}Border.png`;
-    borders[i].id = color;
-    borders[i].onclick = select;
-}
+let nooks = ["nook"];
+let leoniers = ["leonier"];
+let nibblys = ["nibbly"];
+let razors = ["razor"];
 
 function init()
 {
-    div.appendChild(char);
-    for(let character of characters)
+    char.innerHTML = "Select Character";
+    char.classList = "blo";
+    bord.innerHTML = "Select Boarder";
+    bord.classList = "blo";
+    go.innerHTML = "Go!";
+    go.classList = "blo";
+
+    enter.onclick = handleEnterButton;
+    go.onclick = handleGoButton;
+}
+
+function handleEnterButton()
+{
+    currentName = charName.value;
+    currentName = currentName.toLowerCase();
+
+    if(characters.includes(currentName))
     {
-        div.appendChild(character);
+        setUpCharacters(currentName);
+    }
+}
+
+function setUpCharacters(currentName)
+{
+    for(let elem of div.children)
+    {
+        elem.classList = "hide";
+    }
+
+    div.appendChild(char);
+
+    switch(currentName)
+    {
+        case "nook":
+            for(let char of nooks)
+            {
+                let nook = document.createElement("img");
+                nook.id = char;
+                nook.src = `images/map/tokens/${char}.png`;
+                nook.classList = "char";
+                nook.onclick = select;
+                div.appendChild(nook);
+            }
+            break;
+
+        case "leonier":
+            for(let char of leoniers)
+            {
+                let nook = document.createElement("img");
+                nook.id = char;
+                nook.src = `images/map/tokens/${char}.png`;
+                nook.classList = "char";
+                nook.onclick = select;
+                div.appendChild(nook);
+            }
+            break;
+
+        case "razor":
+            for(let char of razors)
+            {
+                let nook = document.createElement("img");
+                nook.id = char;
+                nook.src = `images/map/tokens/${char}.png`;
+                nook.classList = "char";
+                nook.onclick = select;
+                div.appendChild(nook);
+            }
+            break;
+
+        case "nibbly":
+            for(let char of nibblys)
+            {
+                let nook = document.createElement("img");
+                nook.id = char;
+                nook.src = `images/map/tokens/${char}.png`;
+                nook.classList = "char";
+                nook.onclick = select;
+                div.appendChild(nook);
+            }
+            break;
+    }
+
+    addBorders();
+    div.appendChild(go);
+}
+
+function addBorders()
+{
+    for(let i = 0; i < 8; i++)
+    {
+        let color = borders[i];
+        borders[i] = document.createElement("img");
+        borders[i].src = `images/map/tokens/${color}Border.png`;
+        borders[i].id = color;
+        borders[i].classList = "bord";
+        borders[i].onclick = select;
     }
 
     div.appendChild(bord);
@@ -45,30 +118,31 @@ function init()
     {
         div.appendChild(border);
     }
-
-    div.appendChild(go);
 }
 
 function select()
 {
-    if(characters.includes(this))
+    let classL = this.classList.value;
+    if(classL.includes("char"))
     {
-        for(let i = 0; i < 4; i++)
+        let chars = document.getElementsByClassName("char");
+        for(let char of chars)
         {
-            characters[i].classList = "q";
+            char.classList = "char"
         }
 
-        this.classList = "selected";
+        this.classList = "char selected";
     }
 
-    else if(borders.includes(this))
+    else if(classL.includes("bord"))
     {
-        for(let i = 0; i < 8; i++)
+        let bords = document.getElementsByClassName("bord");
+        for(let bord of bords)
         {
-            borders[i].classList = "q";
+            bord.classList = "bord";
         }
-
-        this.classList = "selected";
+        
+        this.classList = "bord selected";
     }
 }
 
