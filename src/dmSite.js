56fe1e51e1be443fea3478574f8a1ca89a44c380
Vi Snyder <ViTheAxolotl@gmail.com>
2024-03-13 19:05:50 -0400

    ope

--- src/dmSite.js
+++ src/dmSite.js
@@ -108,7 +108,7 @@ function handleDeleteOrEdit()
     {
         for(let i = 0; i < 2; i++)
         {
-            currentDiv.removeChild(currentDiv.lastChild);
+            currentDiv.parentElement.removeChild(currentDiv.lastChild);
         }
     }
 
