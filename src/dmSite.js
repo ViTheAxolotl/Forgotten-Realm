91220d35d6691b40a98f1436f2d3e64ecb4ee642
Vi Snyder <ViTheAxolotl@gmail.com>
2024-03-13 22:23:33 -0400

    add each button

--- src/dmSite.js
+++ src/dmSite.js
@@ -215,7 +215,7 @@ function handleEdit()
     txtFeilds[4].value = curCharacter.title;
     txtFeilds[5].value = curCharacter.xPos;
     txtFeilds[6].value = curCharacter.yPos;
-    editDiv.appendChild(edit);
+    buttons.forEach(em => {editDiv.appendChild(em)});
 }
 
 function resetDelete()
