0ddb9dc76a21b8cabf0e6a6b60bf0bc643ff25b2
Vi Snyder <ViTheAxolotl@gmail.com>
2024-04-09 14:39:45 -0400

    make hidden not findable

--- src/map.js
+++ src/map.js
@@ -212,7 +212,6 @@ function addCharacter(character, update)
         char[0].id = character["name"];
         char[0].classList = `tokens ${character["name"]} char`;
         char[1].src = `images/map/tokens/${character["border"]}Border.png`;
-        char[1].title = `${character["name"].charAt(0).toUpperCase() + character["name"].slice(1, letterRemover)}:${character["title"]}`;
         char[1].id = character["border"];
         char[1].classList = `tokens ${character["name"]} border_`;
         char[1].onclick = handleCharClick;
@@ -222,7 +221,12 @@ function addCharacter(character, update)
         char[2].classList = `tokens ${character["name"]} hp`;
         char[2].title = `${character["currentHp"]} ${character["maxHp"]}`;
         let x = pos[0];
-        let y = pos[0]; 
+        let y = pos[0];
+        
+        if(!character["title"].includes("Hidden"))
+        {
+            char[1].title = `${character["name"].charAt(0).toUpperCase() + character["name"].slice(1, letterRemover)}:${character["title"]}`;
+        }
 
         if(htmlInfo[0] == character["name"])
         {
