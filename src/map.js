bb0b0504e0328dc38d24d5fd0271d219602e4ead
Vi Snyder <ViTheAxolotl@gmail.com>
2024-04-12 22:33:28 -0400

    y

--- src/map.js
+++ src/map.js
@@ -311,7 +311,7 @@ function addCharacter(character, update)
                         stuffs[d].classList.add("tokens");
                         stuffs[d].src = char[d].src;
                         stuffs[d].classList.add(character["name"]);
-                        placeTokens(pos[i], x, stuffs[d]);
+                        placeTokens(pos[i], y, stuffs[d]);
 
                         switch(d)
                         {
