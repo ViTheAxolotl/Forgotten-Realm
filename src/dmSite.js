fb2ff934a2808bd73e4086c92996758292553cd3
Vi Snyder <ViTheAxolotl@gmail.com>
2024-05-07 15:22:16 -0400

    time

--- src/dmSite.js
+++ src/dmSite.js
@@ -402,6 +402,7 @@ function handleQuick()
     let curDate = new Date();
     let date = document.createElement("h3");
     date.innerHTML = `Current Hps at time of ${curDate.getTime()}`;
+    div.appendChild(date);
 
     for(let key of Object.keys(wholeData))
     {
