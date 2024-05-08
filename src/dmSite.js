237abd02f44bb3327ac5947875d24f2fe71a3e33
Vi Snyder <ViTheAxolotl@gmail.com>
2024-05-08 16:26:54 -0400

    plz

--- src/dmSite.js
+++ src/dmSite.js
@@ -1,7 +1,7 @@
 "use strict";
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import { getFirestore, setDoc, getDocs, deleteDoc, doc, collection, query } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
-import { uploadTO } from 'https://www.vitheaxolotl.github.io/Forgotten-Realm/src/dmSiteTO.js';
+import { uploadTO } from 'https://vitheaxolotl.github.io/Forgotten-Realm/src/dmSiteTO.js';
 
 const firebaseApp = initializeApp
 ({
