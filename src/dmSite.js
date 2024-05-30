a0a03da7119f39abf474fa5932f131a7c297d959
Vi Snyder <ViTheAxolotl@gmail.com>
2024-05-30 18:38:44 -0400

    ?

--- src/dmSite.js
+++ src/dmSite.js
@@ -1,6 +1,6 @@
 "use strict";
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import {  } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 import { getFirestore, setDoc, getDocs, deleteDoc, doc, collection, query } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
