f95bd775312812d68a69d5badcdd9680dd5ddd4c
Vi Snyder <ViTheAxolotl@gmail.com>
2024-07-17 14:30:02 -0400

    button

--- js/pageSetup.js
+++ js/pageSetup.js
@@ -56,7 +56,7 @@ function navBarSetup()
                 '<li class="nav-item"><a class="nav-link active" aria-current="page" href="' + mainLocation + 'itemIndex.html">Magic Item Index</a></li>' +    
             '</ul>' +
         '</div>' +
-        '<a class="navbar-brand" style="float = right" href="' + mainLocation + 'loginPage.html?' + url.slice(-1) + '">Login</a>',
+        '<a class="navbar-brand" style="float = right" href="' + mainLocation + 'loginPage.html?' + url.slice(-1) + '"><button>Login</button></a>',
     '</div>');
 }
 
