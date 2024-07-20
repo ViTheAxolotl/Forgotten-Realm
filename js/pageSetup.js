ac97b4a8be921f677d257ab8098decbacf8a574f
Vi Snyder <ViTheAxolotl@gmail.com>
2024-07-19 22:09:54 -0400

    ?

--- js/pageSetup.js
+++ js/pageSetup.js
@@ -74,6 +74,8 @@ onAuthStateChanged(auth, (user) => {
             </div>
             <a class="navbar-brand" style="float = right" href="${mainLocation}loginPage.html?${url.slice(-1)}"><button class="link-primary bg-UP-grey">Login</button></a>);`;
     }
+
+    navBarSetup();
 });
 
 function init()
