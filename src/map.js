80133180a670a003bd7cd0d667db447442424b76
Vi Snyder <ViTheAxolotl@gmail.com>
2024-07-19 22:39:24 -0400

    ?

--- src/map.js
+++ src/map.js
@@ -254,7 +254,7 @@ function addCharacter(character, update)
         char[1].src = `images/map/tokens/${character["border"]}Border.png`;
         char[1].id = character["border"];
         char[1].classList = `tokens ${character["id"]} border_`;
-        char[1].onclick = handleCharClick;
+        char[1].onclick = handleCharClick(event);
         char[2].src = getHpImg(character);
         char[2].id = "hp";
         char[2].classList = `tokens ${character["id"]} hp`;
@@ -597,12 +597,12 @@ function getHpImg(character)
     }  
 }
 
-function handleCharClick()
+function handleCharClick(event)
 {
     let name = titleTxt.innerHTML.replaceAll(" ", "").split(":");
     let compName = this.title.replaceAll(" ", "").split(":");
 
-    if(this.ctrlKey)
+    if(event.ctrlKey)
     {
         handleViewTokens(this);
     }
