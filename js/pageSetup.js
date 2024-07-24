b273dbc26e81b4587774be33a582d513e7987e60
Vi Snyder <ViTheAxolotl@gmail.com>
2024-07-23 22:15:08 -0400

    ?

--- js/pageSetup.js
+++ js/pageSetup.js
@@ -2,7 +2,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
-import { toTitleCase } from `./viMethods.js`;
+import { toTitleCase } from './viMethods.js';
 
 const firebaseApp = initializeApp
 ({
