b082883c0e5f1db389fb013a12ad0a5ee5ff2476
Vi Snyder <ViTheAxolotl@gmail.com>
2024-07-24 13:35:33 -0400

    ?

--- js/pageSetup.js
+++ js/pageSetup.js
@@ -139,14 +139,13 @@ function logout()
 
 function discordSetup()
 {
-   /* body[0].innerHTML += `
-        <widgetbot
-        server="1042157480463040613"
-        channel="1042157480463040616"
-        width="800"
-        height="600">
-        </widgetbot>
-        <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>`;*/
+    body[0].innerHTML += `
+        <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+        new Crate({
+        server: '1042157480463040613',
+        channel: '1042157480463040616'
+        })
+        </script>`;
 }
 
 function copyrightSetup()
