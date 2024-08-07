238e78dbf6cb30e0eb7e0ab84f5cf66d2fba5227
Vi Snyder <ViTheAxolotl@gmail.com>
2024-08-06 22:46:31 -0400

    plain distance

--- src/map.js
+++ src/map.js
@@ -103,7 +103,7 @@ function init()
         mapSize = rect.width;
         bumper = 0; //was 9
         distance = mapSize / 14 //Math.round(mapSize / 14);
-        movement = distance - 4;
+        movement = distance //- 4;
     }
 
     else
