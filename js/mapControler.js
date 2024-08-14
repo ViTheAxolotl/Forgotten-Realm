c9f3d9921d704045d798e9f75acf4297d002b495
Vi Snyder <ViTheAxolotl@gmail.com>
2024-08-13 21:45:52 -0400

    silly

--- js/mapControler.js
+++ js/mapControler.js
@@ -1064,7 +1064,7 @@ function handleUseAction()
                 }
             }
 
-            set(ref(database, `playerChar/Vi/responses`), {"ability" : skill, "currentResponse" : lastUse, "toBeat" : toBeat, "castBy" : wholeChar[player]["charName"], "isSpell" : isSpell, "ind" : ind, "castUp" : upcast});
+            set(ref(database, `playerChar/Vi/responses`), {"ability" : skill, "currentResponse" : lastUse, "toBeat" : toBeat, "castBy" : wholeChar[player]["charName"], "isSpell" : isSpell, "ind" : ind, "castUp" : castUp});
 
             display = `${wholeChar[player]["charName"]} cast,\n${lastUse}:\n${useInfo} \nWaiting for others to use the Response Action (Under Actions, Miscs)...`;
 
