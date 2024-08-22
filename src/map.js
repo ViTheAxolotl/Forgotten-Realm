57fbd02dcd404e94fbe16e192c25e4c765338ae5
Vi Snyder <ViTheAxolotl@gmail.com>
2024-08-22 15:45:35 -0400

    center custom?

--- src/map.js
+++ src/map.js
@@ -467,6 +467,13 @@ function addCharacter(character, update)
 
         div.appendChild(char[i]);
     }
+
+    if(char[0].classList.contains("customImg")) //Center
+    {
+        let offset = (parseFloat(char[1].style.width) / 2) - (parseFloat(char[0].style.width) / 2);
+        char[0].style.top = `${parseFloat(char[0].style.top.replace("px", "")) + offSet}px`;
+        char[0].style.left = `${parseFloat(char[0].style.left.replace("px", "")) + offSet}px`;
+    }
 }
 
 function exp(xOrY, title, char)
