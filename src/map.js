4332eff1a582d50399167fee77c979b43854cb48
Vi Snyder <ViTheAxolotl@gmail.com>
2024-09-29 13:23:27 -0400

    ?

--- src/map.js
+++ src/map.js
@@ -270,7 +270,7 @@ function addCharacter(character, update)
     char[1].src = `images/map/tokens/${character["border"]}Border.png`;
     char[1].id = character["border"];
     char[1].classList = `tokens ${character["id"]} border_`;
-    if(char[1].src.includes("invisible")){for(let c of char){c.classList.add("noCircle");}}
+    if(char[1].src.includes("invisible")){for(let c of char){c.classList = `noCircle ${c.classList}`;}}
     char[2].src = getHpImg(character);
     char[2].id = "hp";
     char[2].classList = `tokens ${character["id"]} hp`;
